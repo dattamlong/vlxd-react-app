5478bc072d47983a9e47db03edf25e397379c594
dattamlong <ndat9124@gmail.com>
2022-10-20 19:58:32 +0700

    add show/hide password

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,16 +2,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { useState } from "react";
 import { Eye } from "react-bootstrap-icons";
+import "./Register.css";
 function Register() {
   const [showPassword, setShowPassword] = useState(false);
+  const [showRePassword, setShowRePassword] = useState(false);
+
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
+  };
+  const handleClickShowRePassword = () => {
+    setShowRePassword(!showRePassword);
   };
   return (
     <div className="wrapper">
       <Container>
         <div className="row justify-content-around">
-          <form className="col-md-5 bg-light p-3 my-3">
+          <form className="col-md-5 bg-light p-4 my-3">
             <h1 className="text-center h3 py-3 text-uppercase font-weight-bold">
               Register
             </h1>
@@ -47,21 +53,33 @@ function Register() {
                 placeholder="phone number"
               />
             </div>
-            <div className="form-group has-feedback my-3">
+            <div className="form-group passwordInput my-3">
               <input
                 type={showPassword ? "text" : "password"}
                 className="form-control"
                 required
                 placeholder="password"
               />
+              <div
+                className="iconInsidePassword"
+                onClick={() => handleClickShowPassword()}
+              >
+                <Eye />
+              </div>
             </div>
-            <div className="form-group mt-3">
+            <div className="form-group repasswordInput my-3">
               <input
-                type="password"
+                type={showRePassword ? "text" : "password"}
                 className="form-control"
                 required
                 placeholder="retypepassword"
               />
+              <div
+                className="iconInsidePassword"
+                onClick={() => handleClickShowRePassword()}
+              >
+                <Eye />
+              </div>
             </div>
             <input
               type="button"
